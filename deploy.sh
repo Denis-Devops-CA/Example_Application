12c5a9bc639055c2bb939e48669ada0693174ad4
DenisRegan <denismartinoregan@hotmail.com>
2023-07-18 22:22:58 +0100

    added deployer

--- deploy.sh
+++ deploy.sh
@@ -13,22 +13,3 @@ fi
 #pull down instance from docker hub
 docker pull $IMAGE_NAME
 
-#check if container exists
-CONTAINER_EXISTS=$(docker ps -a | grep node_app )
-if [ "$CONTAINER_EXISTS" ]
-then
-	docker rm node_app
-fi
-
-
-
-#create a container called node_app that is available on port 8443 from our docker image
-docker create -p 8443:8443  --name node_app $IMAGE_NAME
-#write private key to a file
-echo $PRIVATE_KEY > privatekey.pem
-#write the server key to a file
-echo $SERVER > server.crt
-#add the server key to the node-app docker container
-docker cp ./privatekey.pem node_app:/privatekey.pem
-#docker cp ./server.crt node_app:/server.crt
-docker start node_app

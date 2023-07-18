d23680a7665686f9d3e0e499b3c190ffdc7dc9d2
DenisRegan <denismartinoregan@hotmail.com>
2023-07-18 21:58:14 +0100

    added deployer

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 # check if there is an instance running with the image name we are deploying
-CURRENT_INSTANCE=$(docker ps -a -q –-filter ancestor=”$IMAGE_NAME” --format=”{{.ID}}” )
+CURRENT_INSTANCE=$(docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
+
 
 #if instance exists stop the instance
 CONTAINER_EXISTS=$(docker ps -a | grep node_app )

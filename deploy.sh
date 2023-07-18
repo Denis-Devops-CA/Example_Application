e4a3234bdb04f402a81a3ff96f468ca1557160e1
DenisRegan <denismartinoregan@hotmail.com>
2023-07-18 21:50:35 +0100

    added deployer

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ then
    docker rm $(docker stop $CURRENT_INSTANCE)
 fi
 
+
 #create a container called node_app that is available on port 8443 from our docker image
 docker create -p 8443:8443  --name node_app $IMAGE_NAME
 #write private key to a file
